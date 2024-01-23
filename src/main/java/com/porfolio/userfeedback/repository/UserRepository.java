5cce31b01ebf24c9a385317fe652bacefac6ff24
Jhe <jeremiahpasion56@gmail.com>
2024-01-23 18:55:12 +0800

    0.0.1

--- src/main/java/com/porfolio/userfeedback/repository/UserRepository.java
+++ src/main/java/com/porfolio/userfeedback/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.porfolio.userfeedback.repository;
 import com.porfolio.userfeedback.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
