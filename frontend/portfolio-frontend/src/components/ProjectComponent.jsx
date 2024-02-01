88d2a9deb9dcef40fae54acd6163f3bc5f0d2fb2
Jhe <jeremiahpasion56@gmail.com>
2024-02-01 23:21:26 +0800

    update content

--- frontend/portfolio-frontend/src/components/ProjectComponent.jsx
+++ frontend/portfolio-frontend/src/components/ProjectComponent.jsx
@@ -4,63 +4,45 @@ const posts = [
     {
       id: 1,
       title: 'University Chabot Service',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
-      author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
+      description:'Worked on team to develop a web-based Chatbot for a university project, collecting, clearning & augmenting data. Integrated Open LLM using Langchain. ',
+      technology: {
+        language: 'Python, JavaScript, HTML & CSS',
+        frameworks: 'Tensorflow, Flask',
+        },
     },
     {
-        id: 2,
-        title: 'Project Landslide',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-          name: 'Michael Foster',
-          role: 'Co-Founder / CTO',
-          href: '#',
-          imageUrl:
-            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      id: 2,
+      title: 'Project Landslide',
+      description: 'Developed a mobile application featuring a lightweight model to detect post-landslide occurrences. Collaborated with the Mines and Geosciences Bureau to enhance project insight.',
+      technology: {
+        language: 'Java & Python',
+        frameworks: 'Android Studio & Tensorflow',
         },
-      },
+    },
+    {
+      id: 3,
+      title: 'Employee Management System',
+      description:'Learned a new stack based on my experience in java creating a web-based application designed to add, edit & delete employee basic information.',
+      technology: {
+        language: 'Java, SQL, JavaScript, HTML & CSS',
+        frameworks: 'Spring Boot, Vite & TailwindCSS',
+        },
+    },
     // More posts...
   ]
   const ProjectComponent = () => {
     return ( 
-        <div className="bg-white py-24 sm:py-32" id="project">
+        <div className="bg-[#DCF2F1] py-24 sm:py-32" id="project">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Projects</h2>
               <p className="mt-2 text-lg leading-8 text-gray-600">
-                Learn how to grow your business with our expert advice.
+                 Discover highlights from my projects
               </p>
             </div>
             <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               {posts.map((post) => (
                 <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-                  <div className="flex items-center gap-x-4 text-xs">
-                    <time dateTime={post.datetime} className="text-gray-500">
-                      {post.date}
-                    </time>
-                    <a
-                      href={post.category.href}
-                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                    >
-                      {post.category.title}
-                    </a>
-                  </div>
                   <div className="group relative">
                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                       <a href={post.href}>
@@ -71,15 +53,14 @@ const posts = [
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
                   </div>
                   <div className="relative mt-8 flex items-center gap-x-4">
-                    <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
                     <div className="text-sm leading-6">
                       <p className="font-semibold text-gray-900">
-                        <a href={post.author.href}>
+                        <a href={post.technology.href}>
                           <span className="absolute inset-0" />
-                          {post.author.name}
+                          {post.technology.language}
                         </a>
                       </p>
-                      <p className="text-gray-600">{post.author.role}</p>
+                      <p className="text-gray-600">{post.technology.frameworks}</p>
                     </div>
                   </div>
                 </article>
