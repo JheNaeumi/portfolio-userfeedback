dcbab4d5338bb4050073e8d8528aa4002477b401
Jhe <jeremiahpasion56@gmail.com>
2024-02-02 21:11:36 +0800

    modify getAvrgRatings API

--- frontend/portfolio-frontend/src/service/UserfeedbackService.js
+++ frontend/portfolio-frontend/src/service/UserfeedbackService.js
@@ -3,12 +3,11 @@ import axios from "axios";
 const REST_API_BASE_URL ='http://localhost:8080/api/users';
 
 export const listAllRatings = () => {
-    return axios.get(REST_API_BASE_URL + "/all");
+    return axios.get(REST_API_BASE_URL + "/rating");
 
 }
 export const postUserFeedback = (userFeedback) => axios.post(REST_API_BASE_URL, userFeedback);
 
-export const getAvrgRatings =() => {
-    return axios.get(REST_API_BASE_URL + "/all/cal");
-
+export const getAvrgRatings = () => { 
+    return axios.get(REST_API_BASE_URL + "/rating/avg");
 }
