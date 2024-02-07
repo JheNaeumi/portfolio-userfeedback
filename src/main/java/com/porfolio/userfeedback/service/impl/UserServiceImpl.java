5ed9587345596d1e045e0e59c0b7880d72fa050f
Jhe <jeremiahpasion56@gmail.com>
2024-02-07 22:33:10 +0800

    change method_name

--- src/main/java/com/porfolio/userfeedback/service/impl/UserServiceImpl.java
+++ src/main/java/com/porfolio/userfeedback/service/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.porfolio.userfeedback.service.impl;
 
 import com.porfolio.userfeedback.dto.UserDto;
-import com.porfolio.userfeedback.dto.UserDtoAvgRating;
+import com.porfolio.userfeedback.dto.UserDtoRating;
 import com.porfolio.userfeedback.entity.User;
 import com.porfolio.userfeedback.mapper.UserMapper;
 import com.porfolio.userfeedback.repository.UserRepository;
@@ -11,9 +11,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.UndeclaredThrowableException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,9 +29,9 @@ public class UserServiceImpl implements UserService {
     }
     // get All Ratings from Users
     @Override
-    public List<UserDtoAvgRating> getAllRatings() {
+    public List<UserDtoRating> getAllRatings() {
        List<User> users = userRepository.findAllRatings();
-       return users.stream().map((user) -> UserMapper.mapToUserDtoAvgRating(user) ).collect(Collectors.toList());
+       return users.stream().map((user) -> UserMapper.mapToUserDtoRating(user) ).collect(Collectors.toList());
     }
     // get Average Ratings
     @Override
