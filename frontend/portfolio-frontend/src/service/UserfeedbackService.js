4670400833d978192d1788af58ab51ea8cec2a96
Jhe <jeremiahpasion56@gmail.com>
2024-02-08 20:07:11 +0800

    add getCountRatings API

--- frontend/portfolio-frontend/src/service/UserfeedbackService.js
+++ frontend/portfolio-frontend/src/service/UserfeedbackService.js
@@ -4,10 +4,12 @@ const REST_API_BASE_URL ='http://localhost:8080/api/users';
 
 export const listAllRatings = () => {
     return axios.get(REST_API_BASE_URL + "/rating");
-
 }
-export const postUserFeedback = (userFeedback) => axios.post(REST_API_BASE_URL, userFeedback);
-
 export const getAvrgRatings = () => { 
     return axios.get(REST_API_BASE_URL + "/rating/avg");
 }
+export const getCountRatings = () => { 
+    return axios.get(REST_API_BASE_URL + "/rating/count");
+}
+
+export const postUserFeedback = (userFeedback) => axios.post(REST_API_BASE_URL, userFeedback);
