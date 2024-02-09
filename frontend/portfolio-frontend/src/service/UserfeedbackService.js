efceac836b0326975841bd9ce93a1abad66b9520
Jhe <jeremiahpasion56@gmail.com>
2024-02-09 22:32:41 +0800

    update getAvrgCount API

--- frontend/portfolio-frontend/src/service/UserfeedbackService.js
+++ frontend/portfolio-frontend/src/service/UserfeedbackService.js
@@ -5,11 +5,8 @@ const REST_API_BASE_URL ='http://localhost:8080/api/users';
 export const listAllRatings = () => {
     return axios.get(REST_API_BASE_URL + "/rating");
 }
-export const getAvrgRatings = () => { 
-    return axios.get(REST_API_BASE_URL + "/rating/avg");
-}
-export const getCountRatings = () => { 
-    return axios.get(REST_API_BASE_URL + "/rating/count");
+export const getAvrgCountRatings = () => { 
+    return axios.get(REST_API_BASE_URL + "/rating/avg+count");
 }
 
 export const postUserFeedback = (userFeedback) => axios.post(REST_API_BASE_URL, userFeedback);
