d684b25d3f54c7df1d59760214e2110c10683282
Jhe <jeremiahpasion56@gmail.com>
2024-02-16 12:33:51 +0800

    added Stat mapper

--- src/main/java/com/porfolio/userfeedback/mapper/StatMapper.java
+++ src/main/java/com/porfolio/userfeedback/mapper/StatMapper.java
@@ -1,4 +1,13 @@
 package com.porfolio.userfeedback.mapper;
 
+import com.porfolio.userfeedback.dto.StatDto;
+import com.porfolio.userfeedback.entity.Stats;
+
 public class StatMapper {
+    public static StatDto MaptoStatDto(Stats stats){
+        return new StatDto(
+                stats.getViewCount(),
+                stats.getDate()
+        );
+    }
 }
