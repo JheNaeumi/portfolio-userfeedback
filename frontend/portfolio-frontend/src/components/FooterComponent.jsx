d163512568f40153c0ec71fd2c7f1af677e5d282
Jhe <jeremiahpasion56@gmail.com>
2024-02-19 23:23:24 +0800

    update footer

--- frontend/portfolio-frontend/src/components/FooterComponent.jsx
+++ frontend/portfolio-frontend/src/components/FooterComponent.jsx
@@ -3,14 +3,28 @@ import './FooterComponent.css'
 import github from '../assets/github.png'
 import linkedin from '../assets/linkedin.png'
 import facebook from '../assets/facebook-logo.png'
-
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import springlogo from '../assets/spring-svgrepo-com.svg'
 
 const FooterComponent = () => {
   return (
     
-    <footer id="footer">
-      
+    <footer id="footer" >
+      <h1 className='text-gray-900'>Powered By:</h1>
+      <div id="logoo" className="flex flex-row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://spring.io" target="_blank">
+          <img src={springlogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <div className="social-links">
+        <div className="onlysocial-link">
         <a href="https://github.com/JheNaeumi" target="_blank" rel="noopener noreferrer">
           <img src={github} alt="GitHub" />
         </a>
@@ -19,12 +33,16 @@ const FooterComponent = () => {
         </a>
         <a href="https://www.facebook.com/JheNaeumi" target="_blank" rel="noopener noreferrer">
           <img src={facebook} alt="Facebook" />
-        </a>   
-      </div>
-      <span>
+        </a>
+        </div>
+        <span className="allrights">
          All rights reserved
-        </span>
+      </span>   
+      </div>
       
+      
+      
+        
     </footer>
   )
 }
