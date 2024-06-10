b942e7a9ce96f880f8bcd320e1b2d9a200506a4a
Jhe <jeremiahpasion56@gmail.com>
2024-06-10 18:09:01 +0800

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   plugins: [react()],
-  build: {
-    outDir: '../backend/src/main/resources/static/',
-    emptyOutDir: true,
-  }
 })
