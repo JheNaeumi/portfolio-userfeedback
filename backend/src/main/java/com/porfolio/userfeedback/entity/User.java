ba26860bff1c0e15669d8b0733379bd9ecc0e1d5
Jhe <jeremiahpasion56@gmail.com>
2024-06-11 22:28:08 +0800

    update user Entity

--- backend/src/main/java/com/porfolio/userfeedback/entity/User.java
+++ backend/src/main/java/com/porfolio/userfeedback/entity/User.java
@@ -12,10 +12,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="user")
+@Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
     @Column(name = "name")
     private String name;
